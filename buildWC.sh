6526ea58317c9ed3e39f97f491d12144c7e6ce8a
Juri <juri.strumpflohner@gmail.com>
2018-06-15 21:29:28 +0200

    working version

--- buildWC.sh
+++ buildWC.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-ng build ngelements --prod --output-hashing=none && cat dist/ngelements/runtime.js dist/ngelements/polyfills.js dist/ngelements/scripts.js dist/ngelements/main.js > dist/ngelements.js
-cp dist/ngelements.js ./demo
+ng build ngelements --prod --output-hashing=none && cat dist/ngelements/runtime.js dist/ngelements/polyfills.js dist/ngelements/scripts.js dist/ngelements/main.js > demo/ngelements.js
+# cp dist/ngelements.js ./demo

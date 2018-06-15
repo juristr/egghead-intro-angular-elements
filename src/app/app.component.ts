c16dcfb129e11065cdce47e9cdb2b0fc106663bc
Juri <juri.strumpflohner@gmail.com>
2018-06-15 21:37:57 +0200

    implement output mapping

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,10 @@ import { Component, Input } from '@angular/core';
 })
 export class AppComponent {
   @Input() name;
+
+  @Output() greet = new EventEmitter();
+
+  doGreet() {
+    this.greet.emit(`Hi, ${this.name}!`);
+  }
 }

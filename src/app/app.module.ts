b1e7bcbb5e00b69087b2b360c5a7d9723f87ac25
Juri <juri.strumpflohner@gmail.com>
2018-06-16 14:33:35 +0200

    custom build works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, Injector } from '@angular/core';
 import { createCustomElement } from '@angular/elements';
 
-import { AppComponent } from './app.component';
 import { GreeterComponent } from './greeter.component';
 
 @NgModule({
-  declarations: [AppComponent, GreeterComponent],
+  declarations: [GreeterComponent],
   imports: [BrowserModule],
   providers: [],
-  entryComponents: [GreeterComponent],
-  bootstrap: [AppComponent]
+  entryComponents: [GreeterComponent]
 })
 export class AppModule {
   constructor(injector: Injector) {
     const el = createCustomElement(GreeterComponent, { injector: injector });
     customElements.define('do-greet', el);
   }
+
+  ngDoBootstrap() {}
 }

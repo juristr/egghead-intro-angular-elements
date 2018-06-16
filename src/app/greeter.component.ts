710a3a5cfb17af959f8a8cbbb2e2348bfdf72cb6
Juri <juri.strumpflohner@gmail.com>
2018-06-16 14:53:44 +0200

    fix example

--- src/app/greeter.component.ts
+++ src/app/greeter.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'do-greet',
@@ -6,13 +6,19 @@ import { Component, OnInit, Input } from '@angular/core';
     <div>
       Hi, {{ name }}!
     </div>
+    <button (click)="doGreet()">Say Hello</button>
   `,
   styles: []
 })
 export class GreeterComponent implements OnInit {
   @Input() name;
+  @Output() greet = new EventEmitter();
 
   constructor() {}
+
+  doGreet() {
+    this.greet.emit(`Hi, ${this.name}!`);
+  }
 
   ngOnInit() {}
 }

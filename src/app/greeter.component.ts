8926e478952c0f06ce33538bca46e8b02620c907
Juri <juri.strumpflohner@gmail.com>
2018-06-16 08:58:27 +0200

    initial setup

---
+++ src/app/greeter.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'do-greet',
+  template: `
+    <div>
+      Hi there!
+    </div>
+  `,
+  styles: []
+})
+export class GreeterComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}

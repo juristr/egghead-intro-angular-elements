b691d81f391f1906994f1e9158e1a3663d5ebeee
Juri <juri.strumpflohner@gmail.com>
2019-02-10 17:41:46 +0100

    upgrade to latest ngx-build-plus

--- buildWC.sh
+++ buildWC.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-ng build ngelements --prod --output-hashing=none && cat dist/ngelements/runtime.js dist/ngelements/polyfills.js dist/ngelements/scripts.js dist/ngelements/main.js > demo/ngelements.js
-
-# ng build ngelements --prod --output-hashing none
-# mv dist/ngelements/main.js demo/ngelements.js
+ng build ngelements --prod --output-hashing=none --single-bundle true --keep-polyfills true
+mv dist/ngelements/main.js demo/ngelements.js
+mv dist/ngelements/polyfills.js demo
